2394f8317cf47e898e4a82b9bf1ce84ebc98292e
mrdong <1623497521@qq.com>
2014-11-19 18:36:25 +0800

    none

--- list.c
+++ list.c
@@ -1,61 +1,127 @@
 #include "list.h"
 #include <stdio.h>
 #include <stdlib.h>
-void list_add(PLIST *pphead, PLIST link)
+static void node_del(PLIST *ppHead,PLIST pNode);
+PLISTINFO create_list(int nsize,int (*cbcompare)(void *,void *))
 {
-    if(link)
+    PLISTINFO pListInfo = NULL;
+    if(pListInfo = malloc(sizeof(LISTINFO)))
     {
-        if(*pphead)
+        pListInfo->phead = NULL;
+        pListInfo->data_size = nsize;
+        pthread_mutex_init(&pListInfo->lock,NULL);
+        pListInfo->compare = cbcompare;
+    }
+    return pListInfo;
+}
+LIST_STATUS list_add(PLISTINFO pListInfo, void *pData)
+{
+    PLIST pNewNode = NULL;
+    if(pListInfo)
+    {
+        pNewNode = (PLIST)malloc(sizeof(LIST) + pListInfo->data_size);
+        if(pNewNode)
         {
-            link->next = (*pphead);
-            *pphead = link;
+            memcpy(pNewNode->data,pData,pListInfo->data_size);
+            pthread_mutex_lock(&pListInfo->lock);
+            pNewNode->next = pListInfo->phead;
+            pListInfo->phead = pNewNode;
+            pthread_mutex_unlock(&pListInfo->lock);
         }
-        else
-        {
-            *pphead = link;
-            link->next = NULL;
-        }
+    }
+    else
+        return LIST_FAIL;
+    return LIST_SUC;
+}
+//compare 查找对比函数 相等返回0
+PLIST lookup_node(PLISTINFO pListInfo,void *pkey)
+{
+    PLIST pNode = NULL;
+    if(!pListInfo)
+    {
+        return NULL;
+    }
+    pthread_mutex_lock(&pListInfo->lock);
+    if(pListInfo->compare)
+    {
+        pthread_mutex_unlock(&pListInfo->lock);
+        return NULL;
+    }
+    pNode = pListInfo->phead;
+    while(pNode)
+    {
+        if(!pListInfo->compare(pkey,pNode->data))
+            break;
+        pNode = pNode->next;
+    }
+    pthread_mutex_unlock(&pListInfo->lock);
+    return pNode;
+}
 
+LIST_STATUS list_del(PLISTINFO pListInfo,PLIST pNode)
+{
+    PLIST pHead = NULL;
+    if(!pListInfo)
+    {
+        return LIST_FAIL;
+    }
+    pthread_mutex_lock(&pListInfo->lock);
+    pHead = pListInfo->phead;
+    node_del(&pHead,pNode);
+    pthread_mutex_unlock(&pListInfo->lock);
+    return LIST_SUC;
+}
+
+static void node_del(PLIST *ppHead,PLIST pNode)
+{
+    if(*ppHead == pNode)
+    {
+        free(ppHead);
+        *ppHead = NULL;
+    }
+    while(*ppHead)
+    {
+        if((*ppHead)->next == pNode)
+        {
+            (*ppHead)->next = pNode->next;
+            free(pNode);
+        }
+    }
+
+}
+
+static void *list_del_all(void *p)
+{
+    PLIST *ppHead = p,temp = NULL;
+    while(*ppHead)
+    {
+       temp = *ppHead; 
+       free(temp);
+       *ppHead = (*ppHead)->next;
     }
 }
 
-void list_del(PLIST *pphead,PLIST link)
+LIST_STATUS empty_list(PLISTINFO pListInfo)
 {
-    if(*pphead == link)
-    {
-        release_node(*pphead);
-        *pphead = NULL;
-    }
-    while(*pphead)
-    {
-        if((*pphead)->next == link)
-        {
-            (*pphead)->next = link->next;
-            release_node(link);
-        }
-    }
+    pthread_t thid;
+    pthread_attr_t thAttr;
+    PLIST pHead = NULL;
+    PLIST pnxt = NULL;
+    if(!pListInfo)
+        return LIST_SUC;
+    pthread_attr_init(&thAttr);
+    pthread_attr_setdetachstate(&thAttr,PTHREAD_CREATE_DETACHED);
+    pthread_mutex_lock(&pListInfo->lock);
+    pHead = pListInfo->phead;
+    pListInfo->phead = NULL;
+    pthread_create(&thid,&thAttr,list_del_all,&pHead);
 }
-
-void release_node(PLIST node)
+LIST_STATUS release_list(PLISTINFO pListInfo)
 {
-    if(node)
-    {
-        if(node->date)
-        {
-            free(node->date);
-        }
-        free(node);
-    }
-    return ;
-}
-
-void release_list(PLIST head)
-{
-    PLIST nxt = NULL;
-    while(head)
-    {
-        nxt = head->next;
-        release_node(head);
-        head = nxt;
-    }
+    if(!pListInfo)
+        return LIST_SUC;
+    empty_list(pListInfo);
+    pthread_mutex_destroy(&pListInfo->lock);
+    free(pListInfo);
+    return LIST_SUC;
 }
--- list.h
+++ list.h
@@ -1,18 +1,30 @@
 #ifndef __LIST_H__
 #define __LIST_H__
 
+#include <pthread.h>
 typedef struct list{
-    void *date;
     struct list *next;
+    char data[0];
 }LIST,*PLIST;
 
+typedef struct{
+    PLIST phead;
+    pthread_mutex_t lock;
+    int data_size;
+    int (*compare)(void *src,void *dst);
+}LISTINFO,*PLISTINFO;
 
-extern void list_add(PLIST *pphead, PLIST link);
+typedef enum{
+    LIST_SUC,
+    LIST_FAIL,
+}LIST_STATUS;
+extern PLISTINFO create_list(int data_size,int (*compare)(void *,void *));
 
-extern void list_del(PLIST *head,PLIST link);
+extern LIST_STATUS list_add(PLISTINFO pListInfo, void *pData);
 
-extern void release_node(PLIST node);
+extern LIST_STATUS list_del(PLISTINFO pListInfo,PLIST plink);
 
-extern void release_list(PLIST head);
+extern LIST_STATUS release_list(PLISTINFO pListInfo);
 
+extern LIST_STATUS empty_list(PLISTINFO pListInfo);
 #endif
